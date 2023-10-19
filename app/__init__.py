4d2b3429c3ee6afa0d40a33987f6ce903f0feb9e
Winslow <owedwards@gmail.com>
2023-10-19 13:13:29 -0400

    initial app startup

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    CORS(app)
+    app.config["CORS_HEADERS"] = "Content-Type"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "SQLALCHEMY_DATABASE_URI")
+    
+    # uncomment once render database has been added to .env
+    # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    #     "RENDER_DATABASE_URI")
+
+    # Import models here for Alembic setup
+    # from app.models.ExampleModel import ExampleModel
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register Blueprints here
+    # from .routes import example_bp
+
+    # app.register_blueprint(example_bp)
+    
+    return app
