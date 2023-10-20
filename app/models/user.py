749029824833067d049203d42b9b04f481cd9dbc
Winslow <owedwards@gmail.com>
2023-10-20 14:36:10 -0400

    Added phone_number, update pass naming

--- app/models/user.py
+++ app/models/user.py
@@ -3,13 +3,15 @@ from app import db
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    hashed_password = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String, unique=True, nullable=False)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
 
-    def __init__(self, email, password, first_name, last_name):
+    def __init__(self, email, hashed_password, phone_number, first_name, last_name):
         self.email = email
-        self.password = password
+        self.hashed_password = hashed_password
+        self.phone_number = phone_number
         self.first_name = first_name
         self.last_name = last_name
 
@@ -17,6 +19,7 @@ class User(db.Model):
         user_dict = {
             "user_id": self.user_id,
             "email": self.email,
+            "phone number": self.phone_number,
             "first_name": self.first_name,
             "last_name": self.last_name
         }
