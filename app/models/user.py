53e400afb3608e16e1ae40d9f6d97c70799e3788
Winslow <owedwards@gmail.com>
2023-10-20 18:28:38 -0400

    removing to_dict method, not in ticket

--- app/models/user.py
+++ app/models/user.py
@@ -14,14 +14,3 @@ class User(db.Model):
         self.phone_number = phone_number
         self.first_name = first_name
         self.last_name = last_name
-
-    def to_dict(self):
-        user_dict = {
-            "user_id": self.user_id,
-            "email": self.email,
-            "phone number": self.phone_number,
-            "first_name": self.first_name,
-            "last_name": self.last_name
-        }
-
-        return user_dict
